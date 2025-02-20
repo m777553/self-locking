dfea9baab14d8a28f762e107d8e7118a87e73fe9
Marina <work.m777553@gmail.com>
2025-02-20 12:14:09 +0300

    STEP-4 add animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import {CONTENT} from "./content.const";
-import {IContent} from "./app.interface";
+import { CONTENT } from "./content.const";
+import { IContent } from "./app.interface";
 
 @Component({
   selector: 'app-root',
@@ -9,16 +9,30 @@ import {IContent} from "./app.interface";
 })
 export class AppComponent {
   title = 'self-locking';
-  content = CONTENT
+  content = CONTENT;
   todaySelfLocking: IContent = {
     image: '🙅‍♂️',
     title: ''
+  };
+  isTextVisible = false; // Для отслеживания видимости текста
+
+  choseSelfLocking() {
+    const iterations = 10; // Количество смен текстов
+    const interval = 100; // Интервал между сменами (в миллисекундах)
+
+    let count = 0;
+    const intervalId = setInterval(() => {
+      const randomIndex = Math.floor(Math.random() * this.content.length);
+      this.todaySelfLocking = { ...this.content[randomIndex] };
+      count++;
+
+      if (count === iterations) {
+        clearInterval(intervalId);
+        this.isTextVisible = true; // Показать последний текст
+      }
+    }, interval);
   }
 
-  choseSelfLocking(){
-    const randomIndex = Math.floor(Math.random() * this.content.length);
-    this.todaySelfLocking = {...this.content[randomIndex]};
-  }
   copyToClipboard() {
     navigator.clipboard.writeText(this.todaySelfLocking.title).then(() => {
       alert('Ваш самозапрет скопирован в буфер обмена!');
